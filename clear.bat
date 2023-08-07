6762f881a1ced6d48c8adda8fb79191ad9c8bce2
xe1os <39219862+xe1os@users.noreply.github.com>
2023-08-07 15:21:07 +0530

    Update clear.bat
    English translation

--- clear.bat
+++ clear.bat
@@ -1,61 +1,66 @@
 @echo off
 setlocal enabledelayedexpansion
-rem 保存当前路径
+
+rem Save the current path
 set currentPath=%~dp0
-rem 保存当前驱动器盘符
+rem Save the current drive letter
 set currentDriver=%~d0
-rem 直接使用批处理文件的相对路径
+rem Change to the directory of the batch file
 %currentDriver%
 cd "%currentPath%"
 
-rem 指定mysql的基础安装路径
+rem Specify the base installation path for MySQL
 set mysql_basedir=%currentPath%
-rem 指定mysql的bin目录
+rem Specify the bin directory for MySQL
 set mysql_bin=%mysql_basedir%\bin
 
 if not exist "%mysql_bin%\mysqld.exe" (
-	echo 请确认参数mysql_basedir，没找到%mysql_bin%\mysqld.exe
-	goto success_exit
+    echo Please confirm the mysql_basedir parameter, mysqld.exe not found in %mysql_bin%
+    goto success_exit
 )
 
 if not exist "%mysql_bin%\mysql.exe" (
-	echo 请确认参数mysql_basedir，没找到%mysql_bin%\mysql.exe
-	goto success_exit
+    echo Please confirm the mysql_basedir parameter, mysql.exe not found in %mysql_bin%
+    goto success_exit
 )
 
-rem 清除数据
-if exist "%currentPath%\UnInstall.bat" (
-	call "%currentPath%\UnInstall.bat"
-	del /q "%currentPath%\UnInstall.bat"
+rem Clear data
+if exist "%currentPath%\uninstall_service.bat" (
+    call "%currentPath%\uninstall_service.bat"
+    del /q "%currentPath%\uninstall_service.bat"
 )
 
-if exist "%currentPath%\Install.bat" (
-	del /q "%currentPath%\Install.bat"
+if exist "%currentPath%\install_service.bat" (
+    del /q "%currentPath%\install_service.bat"
 )
 
-if exist "%currentPath%\start.bat" (
-	del /q "%currentPath%\start.bat"
+if exist "%currentPath%\start_service.bat" (
+    del /q "%currentPath%\start_service.bat"
 )
 
-if exist "%currentPath%\stop.bat" (
-	del /q "%currentPath%\stop.bat"
+if exist "%currentPath%\stop_service.bat" (
+    del /q "%currentPath%\stop_service.bat"
 )
 
 if exist "%currentPath%\reset_root_password.bat" (
-	del /q "%currentPath%\reset_root_password.bat"
+    del /q "%currentPath%\reset_root_password.bat"
+)
+
+if exist "%currentPath%\get5.bat" (
+    del /q "%currentPath%\get5.bat"
 )
 
 if exist "%currentPath%\my.ini" (
-	del /q "%currentPath%\my.ini"
+    del /q "%currentPath%\my.ini"
 )
 
 if exist "%currentPath%\my.cnf" (
-	del /q "%currentPath%\my.cnf"
+    del /q "%currentPath%\my.cnf"
 )
 
 if exist "%currentPath%data" (
-	del /q /s "%currentPath%data"
-	rd /q /s "%currentPath%data"
+    del /q /s "%currentPath%data"
+    rd /q /s "%currentPath%data"
 )
 
 :success_exit
